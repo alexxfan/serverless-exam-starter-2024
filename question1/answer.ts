e03b4af6e7a4094131893b3ed4712252515d66a2
alexxfan <alexxfan>
2024-12-09 12:10:04 +0000

    Function to search for matching transporters

--- question1/answer.ts
+++ question1/answer.ts
@@ -1,1 +1,64 @@
-console.log('Hello world')
+interface Car {
+    make: string;
+    color: string;
+    registration: string;
+    owner: string;
+  }
+  
+  interface Bicycle {
+    make: string;
+    color: string;
+    owner: string;
+  }
+  
+  const carpark: Car[] = [
+   {
+      make: "Toyota Yaris",
+      color: "Red",
+      registration: "231WD1234",
+      owner: "Jane Smith",
+   },
+   {
+      make: "Suzuki Swift",
+      color: "Blue",
+      registration: "241WD4321",
+      owner: "Paul O Regan",
+   },
+   {
+      make: "Ford Puma",
+      color: "Blue",
+      registration: "241WD1212",
+      owner: "Eileen Silk",
+   },
+  ];
+  
+  const bicycleShed: Bicycle[] = [
+   {
+      make: "Revel Rascal XO",
+      color: "Blue",
+      owner: "Cindy Tamoka",
+   },
+   {
+      make: "Yeti SB140 LR",
+      color: "Red",
+      owner: " ",
+   },
+  ];
+
+  function findMatch<T>( data : T[], criteria: (d: T) => boolean ) : T | undefined {
+    return data.find((criteria))
+}
+  
+  function getCarMatches(data: Car[], criteria: (t: Car) => boolean): Car[] {
+    return data.filter(criteria);
+  }
+  
+  function getBicycleMatches(
+    data: Bicycle[],
+    criteria: (t: Bicycle) => boolean
+  ): Bicycle[] {
+    return data.filter(criteria);
+  }
+
+console.log(findMatch<Bicycle>(bicycleShed, (b) => b.color == "Blue"  ))
+console.log(findMatch<Car>(carpark, (b) => b.color == "Red"  ))
